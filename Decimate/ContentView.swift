6a6afbe41ddc38d9cd9b8a15baa54ebdf787af9d
Matt Safaii <3858718+mattsafaii@users.noreply.github.com>
2026-06-05 23:54:25 -0700

    Scaffold Xcode project: SwiftUI Mac app, python/ resources, requirements.txt

---
+++ Decimate/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Decimate")
+            .frame(minWidth: 800, minHeight: 600)
+    }
+}
+
+#Preview {
+    ContentView()
+}
